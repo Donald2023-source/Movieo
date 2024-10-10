ce797f92bc8f0dfe41eeaf917977942911b816ae
Donald2023-source <donalddyusuf@gmail.com>
2024-10-10 05:10:48 -0700

    name

--- src/components/VideoPlay.jsx
+++ src/components/VideoPlay.jsx
@@ -5,10 +5,10 @@ const VideoPlay = ({data, close, media_type}) => {
     return (
         <section className="fixed bg-neutral-700 right-0 top-0 flex justify-center items-center bottom-0 left-0 bg-opacity-50 z-50">
             <div className="bg-black w-full max-h-[50vh] max-w-screen-lg aspect-video rounded relative"></div>
-
+                <h2>My name is Bridget</h2>
             <button className="text-2xl absolute right-20 top-16 z-50" onClick={close}><IoClose/></button>
 
-            <iframe src={`https://www.youtube.com/embed/${videoData?.results[0]?.key}`} className="w-full h-'full"/>
+            <iframe src={`https://www.youtube.com/embed/${videoData?.results[0]?.key}`} className="w-full text-2xl h-'full"/>
         </section>
     )
 }
